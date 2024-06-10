c7014e90da573efd26bee12a9faefac073836838
Nguyen Dinh Cuong <84661482+sashaNull@users.noreply.github.com>
2024-06-10 01:34:48 -0400

    Change deploy script

--- deploy.bat
+++ deploy.bat
@@ -3,12 +3,15 @@ echo Starting MongoDB...
 start "" "C:\Program Files\MongoDB\Server\7.0\bin\mongod" --dbpath ./db_directory
 timeout /t 10
 
+echo Changing directory to admin...
+cd /d %~dp0admin
+
 echo Starting admin.js...
-start "" "node" .\admin\admin.js
+start "" "node" admin.js
 timeout /t 6
 
 echo Starting api.js...
-start "" "node" .\admin\api.js
+start "" "node" api.js
 
 echo Deployment started successfully.
 pause
