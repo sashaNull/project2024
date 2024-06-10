947ad05c72d6de8bcbb9786db7699d51369fe177
Nguyen Dinh Cuong <84661482+sashaNull@users.noreply.github.com>
2024-06-10 01:48:26 -0400

    Update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Starting MongoDB...
 start "" "C:\Program Files\MongoDB\Server\7.0\bin\mongod" --dbpath ./db_directory
-timeout /t 10
+timeout /t 5
 
 echo Changing directory to admin...
 cd /d %~dp0admin
 
 echo Starting admin.js...
 start "" "node" admin.js
-timeout /t 6
+timeout /t 3
 
 echo Starting api.js...
 start "" "node" api.js
